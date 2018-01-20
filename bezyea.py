f93652d5888a874937dbffa18f4f7e503501959b
Guy Tene <guyte149@gmail.com>
2018-01-20 18:54:10 +0200

    solve some problems

--- bezyea.py
+++ bezyea.py
@@ -12,16 +12,15 @@ class CubicBezierCurves(object):
     def __call__(self, *args, **kwargs):
         t = args[0]
         omt = 1 - t
-        return (self.p0 * omt * omt * omt) + (self.c0 * 3 * omt * omt * t) + (self.c1 * 3 * omt * t * t) + (
-        self.p1 * t * t * t)
+        return (self.p0 * omt * omt * omt) + (self.c0 * 3 * omt * omt * t) + (self.c1 * 3 * omt * t * t) + (self.p1 * t * t * t)
 
     def bezier_derivative(self, t):
         omt = 1 - t
-        return 3 * omt * omt *(self.c0 - self.p0) + 6 * omt * t * (self.c1 - self.c0) + 3 * t * t * t * (self.p1 - self.c1)
+        return 3 * omt * omt * (self.c0 - self.p0) + 6 * omt * t * (self.c1 - self.c0) + 3 * t * t * t * (self.p1 - self.c1)
 
     def second_bezier_derivative(self, t):
         omt = 1-t
-        return 6 * omt *(self.c1 - (2 * self.c0) + self.p0) + 6 * t *(self.p1 - (2 * self.c1) + self.c1)
+        return 6 * omt * (self.c1 - (2 * self.c0) + self.p0) + 6 * t * (self.p1 - (2 * self.c1) + self.c1)
 
     def get_curvature(self, t):
         x_tag = self.bezier_derivative(t)[0]
@@ -31,7 +30,7 @@ class CubicBezierCurves(object):
 
         return (x_tag * y_tagai - y_tag*x_tagai) / np.power((x_tag*x_tag) + (y_tag*y_tag), 1.5)
 
-    def get_curve_Length(self):
+    def get_curve_length(self):
         l = 0
         for t in xrange(0, 101):
             if t/100.0 < 1:
@@ -43,15 +42,14 @@ class CubicBezierCurves(object):
     def get_equal_arcs(self, arc_length):
         p0 = self(0)
         t = 0
-        list = [p0]
+        lst = [p0]
         for i in xrange(0, 1001):
             p1 = self(t + i/1000)
             if np.linalg.norm(p1 - p0) >= arc_length:
-                list.append(p1)
+                lst.append(p1)
                 p0 = p1
                 t = t + i
-        return list
-
+        return lst
 
     def find_parallel(self, t, width):
         der_x = self.bezier_derivative(t)[0]
@@ -71,6 +69,5 @@ class CubicBezierCurves(object):
 
         return CubicBezierCurves(p0, c0, c1, p1)
 
-
     def __str__(self):
-        return 'p0- {} \nc0- {} \nc1- {} \np1- {} \nlength {}'.format(self.p0, self.c0, self.c1, self.p1, self.get_curve_Length())
+        return 'p0- {} \nc0- {} \nc1- {} \np1- {} \nlength {}'.format(self.p0, self.c0, self.c1, self.p1, self.get_curve_length())
