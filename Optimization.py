912fe72f69226cfad3f706090ead43fcfed24457
EranOvadia <eran.ovadia8@gmail.com>
2018-11-11 12:56:33 +0200

    fixed master conflict

---
+++ Optimization.py
@@ -0,0 +1,256 @@
+from bezyea import *
+import numpy as np
+
+
+class Optimization(object):
+
+    def __init__(self, p0, p1, best_curve):
+        # l = np.linalg.norm(p1 - p0)
+        self.p0 = p0
+        self.p1 = p1
+        self.best_curve = best_curve
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplemented
+
+    def __str__(self):
+        raise NotImplemented
+
+
+class RandomSearch(Optimization):
+    def __init__(self, p0, p1, best_curve):
+        super(RandomSearch, self).__init__(p0, p1, best_curve)
+        # l = np.linalg.norm(p1 - p0)
+        self.p0 = p0
+        self.p1 = p1
+        # self.vector_multiplier0 = vector_multiplier0
+        # self.vector_multiplier1 = vector_multiplier1
+        # self.random_length = random_length
+        self.best_curve = best_curve
+
+    def __call__(self, *args, **kwargs):
+        t = args[0]
+        return self.best_curve(t)
+
+    @staticmethod
+    def first_curve(p0, ang0, p1, ang1):
+        best_curve = QuanticBezierCurve.create_curve(p0, ang0, p1, ang1)
+        return best_curve
+
+    @staticmethod
+    def next_curve(p0, ang0, p1, ang1):
+        l = np.linalg.norm(p1 - p0)
+
+        random_length = 0.1 * l / sqrt(2)
+
+        best_curve = RandomSearch.first_curve(p0, ang0, p1, ang1)
+        best_rate = best_curve.rate_curve(best_curve)
+
+        v0 = np.array([np.cos(np.deg2rad(ang0)), np.sin(np.deg2rad(ang0))])
+        v1 = np.array([np.cos(np.deg2rad(ang1)), np.sin(np.deg2rad(ang1))])
+
+        new_vector_multiplier0 = (best_curve.c0 - best_curve.p0) / v0 + uniform(-random_length, random_length)
+        new_vector_multiplier1 = (best_curve.c3 - best_curve.p1) / v1 + uniform(-random_length, random_length)
+
+        new_c1 = best_curve.c1 + np.array(
+            [uniform(-random_length, random_length), uniform(-random_length, random_length)])
+
+        new_c2 = best_curve.c2 + np.array(
+            [uniform(-random_length, random_length), uniform(-random_length, random_length)])
+        new_curve = QuanticBezierCurve(p0, p0 + new_vector_multiplier0 * l * v0, new_c1, new_c2, p1 +
+                                       new_vector_multiplier1 * l * v1, p1)
+
+        new_rate = new_curve.rate_curve(new_curve)
+
+        if 0 < new_rate < best_rate and new_vector_multiplier0 > 0 > new_vector_multiplier1:
+            best_curve = new_curve
+
+        return best_curve
+
+    @staticmethod
+    def last_curve(p0, ang0, p1, ang1, res=175):
+        best_curve = RandomSearch.first_curve(p0, ang0, p1, ang1)
+        for i in range(res):
+            best_curve = RandomSearch.next_curve(p0, ang0, p1, ang1)
+
+        return RandomSearch(p0, p1, best_curve)
+
+    def __str__(self):
+        return self.best_curve
+
+
+class ParticleSwarm(Optimization):
+    def __init__(self, p0, p1, best_curve, start_curves, curves):
+        super(ParticleSwarm, self).__init__(p0, p1, best_curve)
+        self.p0 = p0
+        self.p1 = p1
+        self.start_curves = start_curves
+        self.curves = curves
+        self.best_curve = best_curve
+
+    def __call__(self, *args, **kwargs):
+        # i = args[0]
+        return self.curves
+
+    @staticmethod
+    def start_curves(p0, ang0, p1, ang1, res=20):
+        curves = []
+        l = np.linalg.norm(p1 - p0)
+
+        random_length_x = []
+        random_length_y = []
+
+        if ang0 - ang1 > 0:
+            if p0[0] < p1[0] and p0[1] < p1[1]:
+                random_length_x.append(p0[0])
+                random_length_x.append(p1[0])
+                random_length_y.append(p0[1])
+                random_length_y.append(p1[1])
+
+            elif p0[0] > p1[0] and p0[1] < p1[1]:
+                random_length_x.append(p1[0] - abs(p1[0] - p0[0]) - abs(p1[1] - p0[1]))
+                random_length_x.append(p0[0])
+                random_length_y.append(p0[1])
+                random_length_y.append(p1[1])
+
+            elif p0[0] < p1[0] and p0[1] > p1[1]:
+                random_length_x.append(p0[0])
+                random_length_x.append(p1[0])
+                random_length_y.append(p1[1])
+                random_length_y.append(p0[1] + abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]))
+
+            elif p0[0] > p1[0] and p0[1] > p1[1]:
+                random_length_x.append(p1[0] - abs(p1[0] - p0[0]) - abs(p1[1] - p0[1]))
+                random_length_x.append(p0[0])
+                random_length_y.append(p1[1])
+                random_length_y.append(p0[1] + abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]))
+
+        elif ang0 - ang1 < 0:
+            if p0[0] < p1[0] and p0[1] < p1[1]:
+                random_length_x.append(p0[0])
+                random_length_x.append(p1[0] + abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]))
+                random_length_y.append(p0[1] - abs(p1[0] - p0[0]) - abs(p1[1] - p0[1]))
+                random_length_y.append(p1[1])
+
+            elif p0[0] > p1[0] and p0[1] < p1[1]:
+                random_length_x.append(p1[0])
+                random_length_x.append(p0[0])
+                random_length_y.append(p0[1])
+                random_length_y.append(p1[1])
+
+            elif p0[0] < p1[0] and p0[1] > p1[1]:
+                random_length_x.append(p0[0])
+                random_length_x.append(p1[0] + abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]))
+                random_length_y.append(p1[1])
+                random_length_y.append(p0[1])
+
+            elif p0[0] > p1[0] and p0[1] > p1[1]:
+                random_length_x.append(p1[0] - abs(p1[0] - p0[0]) - abs(p1[1] - p0[1]))
+                random_length_x.append(p0[0])
+                random_length_y.append(p1[1])
+                random_length_y.append(p0[1])
+
+        elif ang0 == ang1:
+            if p0[0] < p1[0] and p0[1] < p1[1]:
+                random_length_x.append(p0[0])
+                random_length_x.append(p1[0])
+                random_length_y.append(p0[1])
+                random_length_y.append(p1[1])
+
+            elif p0[0] < p1[0] and p0[1] > p1[1]:
+                random_length_x.append(p0[0])
+                random_length_x.append(p1[0])
+                random_length_y.append(p1[1] - abs(p1[0] - p0[0]) - abs(p1[1] - p0[1]))
+                random_length_y.append(p0[1] + abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]))
+
+            elif p0[0] > p1[0] and p0[1] < p1[1]:
+                random_length_x.append(p1[0])
+                random_length_x.append(p0[0])
+                random_length_y.append(p0[1])
+                random_length_y.append(p1[1])
+
+            elif p0[0] > p1[0] and p0[1] > p1[1]:
+                random_length_x.append(p1[0])
+                random_length_x.append(p0[0])
+                random_length_y.append(p1[1] - abs(p1[0] - p0[0]) - abs(p1[1] - p0[1]))
+                random_length_y.append(p0[1] + abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]))
+
+        v0 = np.array([np.cos(np.deg2rad(ang0)), np.sin(np.deg2rad(ang0))])
+        v1 = np.array([np.cos(np.deg2rad(ang1)), np.sin(np.deg2rad(ang1))])
+
+        for i in range(0, res, 1):
+            u0 = uniform(0.0, random_length_x[1] - random_length_x[0])
+            u1 = uniform(0.0, random_length_x[1] - random_length_x[0])
+
+            c0 = p0 + u0 * v0
+            c1 = np.array([uniform(random_length_x[0], random_length_x[1]), uniform(random_length_y[0],
+                                                                                    random_length_y[1])])
+            c2 = np.array([uniform(random_length_x[0], random_length_x[1]), uniform(random_length_y[0],
+                                                                                    random_length_y[1])])
+            c3 = p1 - u1 * v1
+            curves.append(QuanticBezierCurve(p0, c0, c1, c2, c3, p1))
+
+        return curves
+
+    @staticmethod
+    def next_curves(curves, ang0, ang1):
+
+        v0 = np.array([np.cos(np.deg2rad(ang0)), np.sin(np.deg2rad(ang0))])
+        v1 = np.array([np.cos(np.deg2rad(ang1)), np.sin(np.deg2rad(ang1))])
+
+        best_rate = 9999.0
+
+        best_curve = None
+
+        for i in range(len(curves)):
+            new_rate = curves[i].rate_curve(curves[i])
+            if best_rate > new_rate:
+                best_rate = new_rate
+                best_curve = curves[i]
+
+        for j in range(0, len(curves), 1):
+            if best_curve != curves[j]:
+                random_length_c0 = (best_curve.c0 - best_curve.p0) / v0 - (curves[j].c0 - curves[j].p0) / v0
+                random_length_c1_x = best_curve.c1[0] - curves[j].c1[0]
+                random_length_c1_y = best_curve.c1[1] - curves[j].c1[1]
+                random_length_c2_x = best_curve.c2[0] - curves[j].c2[0]
+                random_length_c2_y = best_curve.c2[1] - curves[j].c2[1]
+                random_length_c3 = (best_curve.c3 - best_curve.p1) / v1 - (curves[j].c3 - curves[j].p1) / v1
+
+                plus_c0 = uniform(random_length_c0 * 0.1, random_length_c0 * 0.375) * v0
+                plus_c1 = np.array([uniform(random_length_c1_x * 0.1, random_length_c1_x * 0.375),
+                                    uniform(random_length_c1_y * 0.1, random_length_c1_y * 0.375)])
+                plus_c2 = np.array([uniform(random_length_c2_x * 0.1, random_length_c2_x * 0.375),
+                                    uniform(random_length_c2_y * 0.1, random_length_c2_y * 0.375)])
+                plus_c3 = uniform(random_length_c3 * 0.1, random_length_c3 * 0.375) * v1
+
+                curves[j] = QuanticBezierCurve(curves[j].p0, curves[j].c0 + plus_c0, curves[j].c1 + plus_c1,
+                                               curves[j].c2 + plus_c2, curves[j].c3 + plus_c3,
+                                               curves[j].p1)
+                new_rate = curves[j].rate_curve(curves[j])
+
+                if best_rate > new_rate:
+                    best_rate = new_rate
+                    best_curve = curves[j]
+
+        return curves
+
+    @staticmethod
+    def last_curves(p0, ang0, p1, ang1, res=15):
+        curves = [ParticleSwarm.start_curves(p0, ang0, p1, ang1)]
+        for i in range(1, res, 1):
+            curves.append(ParticleSwarm.next_curves(curves[i-1], ang0, ang1))
+
+        best_rate = 9999
+        best_curve = None
+
+        for i in range(0, len(curves[res - 1]), 1):
+            new_rate = curves[res - 1][i].rate_curve(curves[res - 1][i])
+            if 0 < new_rate < best_rate:
+                best_rate = new_rate
+                best_curve = curves[res - 1][i]
+
+        return ParticleSwarm(p0, p1, best_curve, curves[0], curves[res - 1])
+
+    def __str__(self):
+        return self.curves
--- bezyea.py
+++ bezyea.py
@@ -1,13 +1,23 @@
+from random import *
+from __builtin__ import xrange
+
 import numpy as np
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+
 from math import *
+
+from matplotlib.lines import Line2D
+from pyparsing import range
+
 from trajectory import *
 from progress.bar import Bar
 
 
 class BezierCurve(object):
-    def __init__(self):
-        pass
+    def __init__(self, p0, p1):
+        self.p0 = p0
+        self.p1 = p1
 
     def __call__(self, *args, **kwargs):
         raise NotImplemented
@@ -40,17 +50,45 @@ class BezierCurve(object):
         #     number = 6.9533558078350043e-310
         return number
 
-    def get_max_curvature_change(self, res=1000):
-        max_curvature_change = 0
-        for i in range(0, res - 1, 1):
+    def get_max_curvature(self):
+        l = np.linalg.norm(self.p1 - self.p0)
+        res = 15.0 / (sqrt(2)) * l
+        max_curvature = 0.0
+        for i in np.arange(0.0, res + 1.0, 1.0):
+            new_curvature = abs(self.get_curvature(i / res))
+            if max_curvature < new_curvature:
+                max_curvature = new_curvature
+        return max_curvature
+
+    def get_max_curvature_change(self, res=15.0):
+        l = np.linalg.norm(self.p1 - self.p0)
+        res = 15.0 / (sqrt(2)) * l
+        max_curvature_change = 0.0
+        for i in np.arange(0.0, res - 1.0, 1.0):
             absolute_curvature_change = abs(self.get_curvature((i + 1) / res)) - abs(self.get_curvature(i / res))
             if absolute_curvature_change > max_curvature_change:
                 max_curvature_change = absolute_curvature_change
         return max_curvature_change
 
-    def get_average_curvature_change(self, res=1000):
-        sum_curvature_change = 0
-        for i in range(0, res - 1, 1):
+    def get_point_with_max_curvature_change(self, res=15.0):
+        l = np.linalg.norm(self.p1 - self.p0)
+        res = 15.0 / (sqrt(2)) * l
+        max_curvature_change = 0.0
+        point_max0 = np.array([0, 0])
+        point_max1 = np.array([0, 0])
+        for i in np.arange(0.0, res - 1.0, 1.0):
+            absolute_curvature_change = abs(self.get_curvature((i + 1) / res)) - abs(self.get_curvature(i / res))
+            if absolute_curvature_change > max_curvature_change:
+                max_curvature_change = absolute_curvature_change
+                point_max0 = self((i + 1) / res)
+                point_max1 = self(i / res)
+        return point_max0, point_max1
+
+    def get_average_curvature_change(self, res=15.0):
+        l = np.linalg.norm(self.p1 - self.p0)
+        res = 15.0 / (sqrt(2)) * l
+        sum_curvature_change = 0.0
+        for i in np.arange(0, res - 1.0, 1.0):
             absolute_curvature_change = abs(self.get_curvature((i + 1) / res)) - abs(self.get_curvature(i / res))
             sum_curvature_change += absolute_curvature_change
         average_curvature_change = sum_curvature_change / res
@@ -96,10 +134,10 @@ class BezierCurve(object):
 
     def get_setpoints(self, arc_length, start_poistion=0, step=0.0000025):
         """
-        return a list of Setpoints, one arc_length apart from each other
-        :param arc_length:
-        :return:
-        """
+                                return a list of Setpoints, one arc_length apart from each other
+                                :param arc_length:
+                                :return:
+                                """
         p0 = self(0)
         last_curvature = 0
         position = start_poistion
@@ -156,7 +194,7 @@ class BezierCurve(object):
 
 class CubicBezierCurve(BezierCurve):
     def __init__(self, p0, c0, c1, p1):
-        super(CubicBezierCurve, self).__init__()
+        super(CubicBezierCurve, self).__init__(p0, p1)
         self.p0 = p0
         self.p1 = p1
         self.c0 = c0
@@ -166,7 +204,7 @@ class CubicBezierCurve(BezierCurve):
         t = args[0]
         omt = 1 - t
         return (self.p0 * omt * omt * omt) + (self.c0 * 3 * omt * omt * t) + (self.c1 * 3 * omt * t * t) + (
-            self.p1 * t * t * t)
+                self.p1 * t * t * t)
 
     def bezier_derivative(self, t):
         omt = 1 - t
@@ -196,49 +234,153 @@ class CubicBezierCurve(BezierCurve):
         Q = CubicBezierCurve(curve.p1, q1, q2, q3)
         return Q
 
-
-def __str__(self):
-    return 'p0- {} \nc0- {} \nc1- {} \np1- {} \nlength {}'.format(self.p0, self.c0, self.c1, self.p1,
-                                                                  self.get_curve_length())
+    def __str__(self):
+        return 'p0- {} \nc0- {} \nc1- {} \np1- {} \nlength {}'.format(self.p0, self.c0, self.c1, self.p1,
+                                                                      self.get_curve_length())
 
 
 class BezierPath(object):
     def __init__(self, *curves):
         self.curves = curves
+        self.first_curves = curves
+        # print self.curves
         self.max_v = 2
         self.max_a = 2
+        self.counter = 1
+        self.counter1 = 0
+        self.list_curves_x = [[]]
+        self.list_curves_y = [[]]
+        self.fig = plt.figure()
+        # self.xdata, self.ydata = [], []
+        # self.points = []
+        # self.fig = plt.figure()
+        self.plots = []
+        # for j in range(0, len(self.curves[0]), 1):
+        #     self.plots.append([plt.plot([], [])[0] for _ in range(len(self.curves[0]))])
+        # self.patches = self.plots[0] + self.plots[1]
+        # for j in range(2, len(self.curves[0]), 1):
+        #     self.patches += self.plots[j]
 
     # first argument is t, second argument is curve number
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kwaergs):
         t = args[0]
+        s = args[2]
         seg = args[1]
-        # print 'seg={}   curves seg - {}'.format(seg, self.curves[seg])
+        # print 'seg={}   cur/ves seg - {}'.format(seg, self.curves[seg])
         return self.curves[0][seg](t)
 
     def get_angle(self, t, seg):
-        return self.curves[0][seg].get_angle(t)
+        return self.curves[self.counter1][seg].get_angle(t)
 
     def draw_path(self, res=1000.0):
         x_list = []
         y_list = []
+        # ax = plt.axes(xlim=(0, 2), ylim=(0, 100))
+        # for j in xrange(0, len(self.curves)):
         for s in xrange(0, len(self.curves[0])):
             for t in xrange(0, int(res + 1)):
-                print self.get_angle(t / res, s)
-                x_list.append(self(t / res, s)[0])
-                y_list.append(self(t / res, s)[1])
-                # print 't={},  R={}'.format(t / res, 1 / self.get_curvature(t / res))
+                # print self.get_angle(t / res, s)
+                x_list.append(self(t / res, s, 0)[0])
+                y_list.append(self(t / res, s, 0)[1])
+
+            plt.plot(x_list, y_list)
+            x_list = []
+            y_list = []
 
         plt.axes().set_aspect('equal', 'datalim')
-        plt.plot(x_list, y_list)
+        # plt.plot(x_list, y_list)
+
+        # ani = self.animation_draw()
         plt.show()
 
     def get_setpoints(self, arc_length=0.00005):
         l = []
         start_position = 0
-        for c in self.curves[0]:
+        for c in self.curves[0][0]:
             l = l + c.get_setpoints(arc_length, start_position)
             start_position += l[-1].p
         return l
+
+    def animate_init(self):
+        res = 1000.0
+        x_list = []
+        y_list = []
+        for s in xrange(0, len(self.plots), 1):
+            for t in xrange(0, int(res + 1), 1):
+                # print self.get_angle(t / res, s)
+                x_list.append(self(t / res, s, 0)[0])
+                y_list.append(self(t / res, s, 0)[1])
+
+            for plot in self.plots[s]:
+                plot.set_data([], [])
+        # for t in xrange(0, int(res + 1), 1):
+        #     x_list.append(self(t / res, 0, 0)[0])
+        #     y_list.append(self(t / res, 0, 0)[1])
+        #
+        # for plot in self.plots[0]:
+        #     plot.set_data(x_list, y_list)
+        #
+        # x_list = []
+        # y_list = []
+        #
+        # for t in xrange(0, int(res + 1), 1):
+        #     x_list.append(self(t / res, 1, 0)[0])
+        #     y_list.append(self(t / res, 1, 0)[1])
+        #
+        # for plot in self.plots[1]:
+        #     plot.set_data(x_list, y_list)
+
+        # for plot in self.plots[s]:
+        #     plot.set_data(x_list, y_list)
+        # self.curves = [Optimization.particle_swarm(self.curves[0], 90, 90)]
+
+        return self.patches
+
+    def animation_draw(self):
+        plt.subplot().axis([0, 3, 2, 3])
+        ani = animation.FuncAnimation(self.fig, self.animate, init_func=self.animate_init, frames=100, interval=50,
+                                      blit=True)
+        plt.show()
+        # return ani
+
+    def animate(self, i):
+        if self.counter > 10:
+            for s in range(0, len(self.curves[0]), 1):
+                self.curves[0][s] = Optimization.particle_swarm(self.curves[0], 90, 90)[s]
+            self.counter += 1
+        else:
+            self.curves = self.first_curves
+        res = 1000.0
+        x_list = []
+        y_list = []
+        for s in xrange(0, len(self.plots), 1):
+            for j, plot in enumerate(self.plots[s]):
+                for t in xrange(0, int(res + 1), 1):
+                    # print self.get_angle(t / res, s)
+                    x_list.append(self(t / res, s, 0)[0])
+                    y_list.append(self(t / res, s, 0)[1])
+                plot.set_data(x_list, y_list)
+                x_list = []
+                y_list = []
+        #
+        # for t in xrange(0, int(res + 1), 1):
+        #     x_list.append(self(t / res, 0, 0)[0])
+        #     y_list.append(self(t / res, 0, 0)[1])
+        #
+        # for plot in self.plots[0]:
+        #     plot.set_data(x_list, y_list)
+        #
+        # x_list = []
+        # y_list = []
+        #
+        # for t in xrange(0, int(res + 1), 1):
+        #     x_list.append(self(t / res, 1, 0)[0])
+        #     y_list.append(self(t / res, 1, 0)[1])
+        #
+        # for plot in self.plots[1]:
+        #     plot.set_data(x_list, y_list)
+
+        return self.patches
 
     def __str__(self):
         return self.curves
@@ -246,7 +388,7 @@ class BezierPath(object):
 
 class QuanticBezierCurve(BezierCurve):
     def __init__(self, p0, c0, c1, c2, c3, p1):
-        super(QuanticBezierCurve, self).__init__()
+        super(QuanticBezierCurve, self).__init__(p0, p1)
         self.p0 = p0
         self.c0 = c0
         self.c1 = c1
@@ -258,44 +400,46 @@ class QuanticBezierCurve(BezierCurve):
         t = args[0]
         omt = 1 - t
         return (self.p0 * omt * omt * omt * omt * omt) + (self.c0 * 5 * t * omt * omt * omt * omt) + (
-            self.c1 * 10 * t * t * omt * omt * omt) + (self.c2 * 10 * t * t * t * omt * omt) + (
-                   self.c3 * 5 * t * t * t * t * omt) + (self.p1 * t * t * t * t * t)
+                self.c1 * 10 * t * t * omt * omt * omt) + (self.c2 * 10 * t * t * t * omt * omt) + (
+                       self.c3 * 5 * t * t * t * t * omt) + (self.p1 * t * t * t * t * t)
 
     def bezier_derivative(self, t):
         omt = 1 - t
         return 5 * t * t * t * t * (self.p1 - self.c3) + 20 * omt * t * t * t * (self.c3 - self.c2) + (
-            30 * omt * omt * t * t * (self.c2 - self.c1)) + 20 * omt * omt * omt * t * (self.c1 - self.c0) + (
-                   5 * omt * omt * omt * omt * omt * (self.c0 - self.p0))
+                30 * omt * omt * t * t * (self.c2 - self.c1)) + 20 * omt * omt * omt * t * (self.c1 - self.c0) + (
+                       5 * omt * omt * omt * omt * omt * (self.c0 - self.p0))
 
     def second_bezier_derivative(self, t):
         return 20 * (self.p1 - 5 * self.c3 + 10 * self.c2 - 10 * self.c1 + 5 * self.c0 - self.p0) * t * t * t + (
-            15 * (4 * self.c3 - 16 * self.c2 + 24 * self.c1 - 16 * self.c0 + 4 * self.p0) * t * t) + (
-                   10 * (6 * self.c2 - 18 * self.c1 + 18 * self.c0 - 6 * self.p0) * t) + (
-                   20 * self.c1 - 40 * self.c0 + 20 * self.p0)
+                15 * (4 * self.c3 - 16 * self.c2 + 24 * self.c1 - 16 * self.c0 + 4 * self.p0) * t * t) + (
+                       10 * (6 * self.c2 - 18 * self.c1 + 18 * self.c0 - 6 * self.p0) * t) + (
+                       20 * self.c1 - 40 * self.c0 + 20 * self.p0)
 
     @staticmethod
     def create_curve(p0, ang0, p1, ang1):
         l = np.linalg.norm(p1 - p0)
-        u = 0.3 * l
+        u = 0.275 * l
 
         v0 = np.array([np.cos(np.deg2rad(ang0)), np.sin(np.deg2rad(ang0))])
         v1 = np.array([np.cos(np.deg2rad(ang1)), np.sin(np.deg2rad(ang1))])
 
+        value_x = (p1[0] - p0[0]) * 0.25
+        # value_y = p1[1] - p0[1]
+
         c0 = p0 + u * v0
-        c1 = c0
-        c2 = p1 - u * v1
-        c3 = c2
+        c1 = np.array([c0[0] + value_x, c0[1] + c0[1] / l])
+        c3 = p1 - u * v1
+        c2 = np.array([c3[0] - value_x, c3[1] + c3[1] * -v1[1] / (l * l)])
+        # print c1[1], c2[1]
 
         return QuanticBezierCurve(p0, c0, c1, c2, c3, p1)
 
     @staticmethod
-    def rate_curve(curve, optimal_curve):
-        length_rate = optimal_curve.get_curve_length / curve.get_curve_length
-        max_curvature_change_rate = optimal_curve.get_max_curvature_change / curve.get_max_curvature_change
-        average_curvature_change_rate = optimal_curve.get_average_curvature_change / curve.get_average_curvature_change
-
-        final_rate = (length_rate + max_curvature_change_rate + average_curvature_change_rate) * (100 / 3)
-        return final_rate
+    def rate_curve(curve):
+        return curve.get_max_curvature_change() + abs(curve.get_max_curvature())
+        # return curve.get_max_curvature_change() + abs(curve.get_max_curvature()) + curve.get_curve_length()
+        # return curve.get_max_curvature_change() + abs(curve.get_average_curvature_change())
+        # return abs(curve.get_average_curvature_change())
 
     @staticmethod
     def connect_curve(curve, q3, ang2):
