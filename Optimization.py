e06a389d4ea855d063b442ab8253e22b6f56544b
EranOvadia <eran.ovadia8@gmail.com>
2018-11-13 18:00:38 +0200

    add numpy random and vector connection

--- Optimization.py
+++ Optimization.py
@@ -13,6 +13,11 @@ class Optimization(object):
     def __call__(self, *args, **kwargs):
         raise NotImplemented
 
+    @staticmethod
+    def function_random(min_random, max_random, x):
+        m = max_random - min_random
+        return m * x + min_random
+
     def __str__(self):
         raise NotImplemented
 
@@ -49,20 +54,35 @@ class RandomSearch(Optimization):
         v0 = np.array([np.cos(np.deg2rad(ang0)), np.sin(np.deg2rad(ang0))])
         v1 = np.array([np.cos(np.deg2rad(ang1)), np.sin(np.deg2rad(ang1))])
 
-        new_vector_multiplier0 = (best_curve.c0 - best_curve.p0) / v0 + uniform(-random_length, random_length)
-        new_vector_multiplier1 = (best_curve.c3 - best_curve.p1) / v1 + uniform(-random_length, random_length)
+        min_random = -random_length
+        max_random = random_length
+
+        new_vector_multiplier0 = (best_curve.c0 - best_curve.p0) / v0 + RandomSearch.function_random(min_random,
+                                                                                                     max_random,
+                                                                                                     np.random.random())
+        new_vector_multiplier1 = (best_curve.c3 - best_curve.p1) / v1 + RandomSearch.function_random(min_random,
+                                                                                                     max_random,
+                                                                                                     np.random.random())
 
         new_c1 = best_curve.c1 + np.array(
-            [uniform(-random_length, random_length), uniform(-random_length, random_length)])
+            [RandomSearch.function_random(min_random,
+                                          max_random,
+                                          np.random.random()), RandomSearch.function_random(min_random,
+                                                                                            max_random,
+                                                                                            np.random.random())])
 
         new_c2 = best_curve.c2 + np.array(
-            [uniform(-random_length, random_length), uniform(-random_length, random_length)])
+            [RandomSearch.function_random(min_random,
+                                          max_random,
+                                          np.random.random()), RandomSearch.function_random(min_random,
+                                                                                            max_random,
+                                                                                            np.random.random())])
         new_curve = QuanticBezierCurve(p0, p0 + new_vector_multiplier0 * l * v0, new_c1, new_c2, p1 +
                                        new_vector_multiplier1 * l * v1, p1)
 
         new_rate = new_curve.rate_curve(new_curve)
 
-        if 0 < new_rate < best_rate and new_vector_multiplier0 > 0 > new_vector_multiplier1:
+        if 0 < new_rate < best_rate:
             best_curve = new_curve
 
         return best_curve
@@ -178,15 +198,20 @@ class ParticleSwarm(Optimization):
         v0 = np.array([np.cos(np.deg2rad(ang0)), np.sin(np.deg2rad(ang0))])
         v1 = np.array([np.cos(np.deg2rad(ang1)), np.sin(np.deg2rad(ang1))])
 
+        min_random_x = random_length_x[0]
+        min_random_y = random_length_y[0]
+        max_random_x = random_length_x[1]
+        max_random_y = random_length_y[1]
+
         for i in range(0, res, 1):
-            u0 = uniform(0.0, random_length_x[1] - random_length_x[0])
-            u1 = uniform(0.0, random_length_x[1] - random_length_x[0])
+            u0 = ParticleSwarm.function_random(min_random_x, max_random_x, np.random.random())
+            u1 = ParticleSwarm.function_random(min_random_x, max_random_x, np.random.random())
 
             c0 = p0 + u0 * v0
-            c1 = np.array([uniform(random_length_x[0], random_length_x[1]), uniform(random_length_y[0],
-                                                                                    random_length_y[1])])
-            c2 = np.array([uniform(random_length_x[0], random_length_x[1]), uniform(random_length_y[0],
-                                                                                    random_length_y[1])])
+            c1 = np.array([ParticleSwarm.function_random(min_random_x, max_random_x, np.random.random()),
+                           ParticleSwarm.function_random(min_random_y, max_random_y, np.random.random())])
+            c2 = np.array([ParticleSwarm.function_random(min_random_x, max_random_x, np.random.random()),
+                           ParticleSwarm.function_random(min_random_y, max_random_y, np.random.random())])
             c3 = p1 - u1 * v1
             curves.append(QuanticBezierCurve(p0, c0, c1, c2, c3, p1))
 
@@ -208,21 +233,26 @@ class ParticleSwarm(Optimization):
                 best_rate = new_rate
                 best_curve = curves[i]
 
+        min_random = 0.1
+        max_random = 0.375
+
         for j in range(0, len(curves), 1):
             if best_curve != curves[j]:
                 random_length_c0 = (best_curve.c0 - best_curve.p0) / v0 - (curves[j].c0 - curves[j].p0) / v0
-                random_length_c1_x = best_curve.c1[0] - curves[j].c1[0]
-                random_length_c1_y = best_curve.c1[1] - curves[j].c1[1]
-                random_length_c2_x = best_curve.c2[0] - curves[j].c2[0]
-                random_length_c2_y = best_curve.c2[1] - curves[j].c2[1]
+                random_length_c1 = best_curve.c1 - curves[j].c1
+                random_length_c2 = best_curve.c2 - curves[j].c2
                 random_length_c3 = (best_curve.c3 - best_curve.p1) / v1 - (curves[j].c3 - curves[j].p1) / v1
 
-                plus_c0 = uniform(random_length_c0 * 0.1, random_length_c0 * 0.375) * v0
-                plus_c1 = np.array([uniform(random_length_c1_x * 0.1, random_length_c1_x * 0.375),
-                                    uniform(random_length_c1_y * 0.1, random_length_c1_y * 0.375)])
-                plus_c2 = np.array([uniform(random_length_c2_x * 0.1, random_length_c2_x * 0.375),
-                                    uniform(random_length_c2_y * 0.1, random_length_c2_y * 0.375)])
-                plus_c3 = uniform(random_length_c3 * 0.1, random_length_c3 * 0.375) * v1
+                plus_c0 = ParticleSwarm.function_random(min_random, max_random,
+                                                        np.random.random()) * random_length_c0 * v0
+                plus_c1 = np.array([ParticleSwarm.function_random(min_random, max_random, np.random.random()),
+                                    ParticleSwarm.function_random(min_random, max_random,
+                                                                  np.random.random())]) * random_length_c1
+                plus_c2 = np.array([ParticleSwarm.function_random(min_random, max_random, np.random.random()),
+                                    ParticleSwarm.function_random(min_random, max_random,
+                                                                  np.random.random())]) * random_length_c2
+                plus_c3 = ParticleSwarm.function_random(min_random, max_random,
+                                                        np.random.random()) * random_length_c3 * v1
 
                 curves[j] = QuanticBezierCurve(curves[j].p0, curves[j].c0 + plus_c0, curves[j].c1 + plus_c1,
                                                curves[j].c2 + plus_c2, curves[j].c3 + plus_c3,
@@ -239,7 +269,7 @@ class ParticleSwarm(Optimization):
     def last_curves(p0, ang0, p1, ang1, res=15):
         curves = [ParticleSwarm.start_curves(p0, ang0, p1, ang1)]
         for i in range(1, res, 1):
-            curves.append(ParticleSwarm.next_curves(curves[i-1], ang0, ang1))
+            curves.append(ParticleSwarm.next_curves(curves[i - 1], ang0, ang1))
 
         best_rate = 9999
         best_curve = None
