1703ead29984103a0e1657ca989846483ae7a90c
EranOvadia <eran.ovadia8@gmail.com>
2018-11-15 19:04:01 +0200

    add to connect curve also the start curve need more test

--- Animation.py
+++ Animation.py
@@ -75,3 +75,87 @@ class Animation(object):
                                        frames=10, interval=10, blit=True)
 
         plt.show()
+
+
+class AnimationConnection(object):
+
+    def __init__(self, best_curve, ang0, ang1, *curves):
+        self.best_curve = best_curve
+        self.ang0 = ang0
+        self.ang1 = ang1
+
+        # First set up the figure, the axis, and the plot element we want to animate
+        self.curves = curves
+        self.first_curves = curves
+        self.fig = plt.figure()
+        self.ax = plt.axes(xlim=(-0.5, 5), ylim=(-0.5, 5))
+        self.plot_list = []
+        plotcols = ["black", "red", "blue", "green", "yellow", "pink", "orange", "grey", "brown", "purple", "cyan",
+                    "magenta", "gold", "silver", "turquoise"]
+        for s in xrange(0, len(self.curves[0])):
+            lobj = self.ax.plot([], [], lw=2, color=plotcols[s])[0]
+            self.plot_list.append(lobj)
+        for i in xrange(0, 4):
+            plot_point = self.ax.plot([], [], 'ro')[0]
+            self.plot_list.append(plot_point)
+        self.plot_list.append(self.ax.plot([], [])[0])
+
+    # first argument is t, second argument is curve number
+    def __call__(self, *args, **kwargs):
+        t = args[0]
+        seg = args[1]
+        # print 'seg={}   cur/ves seg - {}'.format(seg, self.curves[seg])
+        return self.curves[0][seg](t)
+
+    # initialization function: plot the background of each frame
+    def init(self):
+        for plot in self.plot_list:
+            plot.set_data([], [])
+        return self.plot_list
+
+    # animation function.  This is called sequentially
+    def animate(self, i):
+
+        curves = ParticleSwarm.next_curves(self.curves[0], self.ang0, self.ang1)[0]
+        best_curve = ParticleSwarm.next_curves(self.curves[0], self.ang0, self.ang1)[1]
+        for j in xrange(0, len(self.curves[0]), 1):
+            self.curves[0][j] = curves[j]
+
+        res = 1000.0
+
+        t_list = np.linspace(0.0, 1.0, res)
+
+        for s in xrange(0, len(self.curves[0])):
+
+            x_list = []
+            y_list = []
+
+            for t in t_list:
+                # print self.get_angle(t / res, s)
+                x_list.append(self(t, s)[0])
+                y_list.append(self(t, s)[1])
+
+            self.plot_list[s].set_data(x_list, y_list)
+
+        # setting data for control points
+        self.plot_list[len(self.curves[0])].set_data(best_curve.c0[0], best_curve.c0[1])
+        self.plot_list[len(self.curves[0]) + 1].set_data(best_curve.c1[0], best_curve.c1[1])
+        self.plot_list[len(self.curves[0]) + 2].set_data(best_curve.c2[0], best_curve.c2[1])
+        self.plot_list[len(self.curves[0]) + 3].set_data(best_curve.c3[0], best_curve.c3[1])
+
+        x_list = []
+        y_list = []
+        for i in xrange(0, int(res + 1), 1):
+            x_list.append(self.best_curve(i / res)[0])
+            y_list.append(self.best_curve(i / res)[1])
+
+        self.plot_list[len(self.curves[0]) + 4].set_data(x_list, y_list)
+
+        return self.plot_list
+
+    def display(self):
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(self.fig, self.animate, init_func=self.init,
+                                       frames=10, interval=10, blit=True)
+
+        plt.show()
