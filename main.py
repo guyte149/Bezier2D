61f6bbe28195c56daebedefc2f0abe81f8928ae9
EranOvadia <eran.ovadia8@gmail.com>
2018-11-16 14:22:50 +0200

    fixed divide in zero

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from Animation import *
 
 p0 = np.array([0, 0])
 ang0 = 90
-p1 = np.array([0.75, 0.75])
+p1 = np.array([1, 1])
 ang1 = 90
-p2 = np.array([2, 1])
-ang2 = 270
+p2 = np.array([2, 2])
+ang2 = 0
 
 # c1 = ParticleSwarm.start_curves(p0, ang0, p1, ang1)
 # anim = Animation(ang0, ang1, c1)
