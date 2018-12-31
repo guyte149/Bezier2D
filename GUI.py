736e404159b84dc2ef6a4a4e8e7e0187c472ce1b
Daniel Eildin <daniel.eidlin@gmail.com>
2018-12-31 10:13:52 +0200

    Correct Angles but not done

--- GUI.py
+++ GUI.py
@@ -11,6 +11,7 @@ class PointStart(object):
         self.canvas = canvas
         self.x = x
         self.y = y
+        self.angle = 0
         self.x_point = 0
         self.y_point = 0
         self.num_curve = num_curve
@@ -51,6 +52,7 @@ class PointStart(object):
         input.append(self.text2.get("1.0", 'end-1c'))
         self.x_point = float(input[0])
         self.y_point = float(input[1])
+        self.angle = float(input[2])
         self.oval = self.canvas.create_oval(self.x_point - 3, self.y_point - 3, self.x_point + 3, self.y_point + 3,
                                             fill="blue")
 
@@ -120,6 +122,10 @@ class Curves(object):
         self.points = [Point(self.master, self.canvas, "red", True, 1, 10 + 1 * 100, 10)]
         self.choose_points()
         self.finish = False
+        self.m0 = 0
+        self.b0 = 0
+        self.m1 = 0
+        self.b1 = 0
 
     def is_finish(self):
         if len(self.points) == 6:
@@ -154,6 +160,27 @@ class Curves(object):
         self.curve = QuanticBezierCurve(curve0.curve.p1, c0, c1, self.points[2](), self.points[3](),
                                         self.start_points[0]())
         return curve0
+
+    def correct_angle(self):
+        # find first start point linear equation
+        x0 = self.start_points[0].x
+        y0 = self.start_points[0].y
+        self.m0 = tan(np.deg2rad(self.start_points[0].angle))
+        self.b0 = y0 - self.m0 * x0
+        # find first control point y parameter
+        x = self.points[0].x
+        y = self.m0 * x + self.b1
+        self.points[0].change_place_for_input(x, y)
+
+        # find second start point linear equation
+        x0 = self.start_points[1].x
+        y0 = self.start_points[1].y
+        self.m1 = tan(np.deg2rad(self.start_points[1].angle))
+        self.b1 = y0 - self.m1 * x0
+        # find second control point y parameter
+        x = self.points[3].x
+        y = self.m1 * x + self.b1
+        self.points[3].change_place_for_input(x, y)
 
 
 class Boards(object):
@@ -211,6 +238,7 @@ class Boards(object):
     def create_canvas(self, res=1000.0):
 
         for seg in xrange(0, len(self.curves), 1):
+            # self.curves[seg].correct_angle()
             if seg == 0:
                 self.curves[seg].create_curve()
             else:
@@ -224,6 +252,7 @@ class Boards(object):
             #                                                   self.curves[seg].curve.c2[1])
             # self.curves[seg].points[3].change_place_for_input(self.curves[seg].curve.c3[0],
             #                                                   self.curves[seg].curve.c3[1])
+
 
         for seg in xrange(0, len(self.curves), 1):
             for t in xrange(0, int(res), 1):
